9cfdf2c26ee0c943a08925d9f1326bfebc598fe3
erdivabp24-design <erdivabp24@gmail.com>
2025-11-07 10:34:34 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const tombolSapa = document.getElementById('sapaButton');//pilih tombol berdasarkan ID-Nya.
+tombolSapa.addEventListener('click',function(){//tambahkan 'event listener'untuk aksi 'click'.
+  alert('Halo!,Thanks ya udah mampir!!');//jalankan kode ini ketika tombol di klik.
+});
